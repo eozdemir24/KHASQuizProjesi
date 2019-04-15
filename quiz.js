6f8571aebb566ed338c07bba53e697005e7eb869
eozdemir24 <e.ozdemir59@gmail.com>
2019-04-15 23:33:33 +0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,32 @@
+
+  var questions = ["<p>"
+            "var grade='A';"<br>
+            "document.write(""Entering switch block"");"<br>
+            "switch (grade)"<br/>
+            "{"<br>
+               "case 'A': document.write(""Good job "");"<br>
+               "break;<br/>"<br>
+            
+               "case 'B': document.write("Pretty good");"<br>
+               "break;<br/>"<br>
+            
+               "case 'C': document.write("Passed");"<br>
+               "break;<br/>"<br>
+            
+               "case 'D': document.write("Not so good");"<br>
+               "break;<br/>"<br>
+            
+               "case 'F': document.write("Failed");"<br>
+               "break;<br/>"<br>
+            
+               "default:  document.write("Unknown grade")<br/>"<br>
+            "}"
+            "document.write("Exiting switch block");"]<br>
+
+          document.getElementById("questions")
+        
+      
+      
+      <p>What is the output of this program?</p>
+
+    
